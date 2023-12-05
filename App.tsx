5029996b749670d8492ee5fb0adc8be4a13a7e1c
Diego <diegoramosgomes5@gmail.com>
2023-12-05 14:49:26 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, View} from 'react-native';
+import {SafeAreaProvider, useSafeAreaInsets} from "react-native-safe-area-context";
+import {createStackNavigator} from "@react-navigation/stack";
+import {Home} from "./pages/Home";
+import {NavigationContainer} from "@react-navigation/native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="light"/>
+      <SafeAreaProvider>
+        <NavigationContainer>
+        <Component/>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Component = () => {
+  const {top} = useSafeAreaInsets()
+  
+  const Stack = createStackNavigator();
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'blue',
+      paddingTop: top
+    },
+  });
+  
+  return (
+    <View style={styles.container}>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </View>)
+}
+
+
---
+++ pages/Home.tsx
@@ -0,0 +1,9 @@
+import {Text, View} from "react-native";
+
+export const Home = () => {
+  return (
+    <View style={{flex: 1, backgroundColor: 'blue'}}>
+      <Text style={{color: 'red'}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque error incidunt vitae. Alias atque doloremque fugiat non perspiciatis, quae temporibus vel. Atque consectetur facilis minus molestiae odit rem sint voluptates.</Text>
+    </View>
+  )
+}
